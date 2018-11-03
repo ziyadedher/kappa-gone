1d856e1518f007ef55c7a56312ba20229e2919c2
Ziyad Edher <ziyad.edher@gmail.com>
2018-11-03 19:03:11 -0400

    Introduce basic setuptools with setup.cfg

---
+++ kappagone/__init__.py
@@ -0,0 +1,6 @@
+"""Entry kappagone module.
+
+Contains meta and project related constants for use throughout the project.
+"""
+
+VERSION = "0.0.0a"
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Setup script for kappagone.
+
+This script is purely a wrapper for calling setuptools to read from the
+`setup.cfg` file in this directory. This file should not be modified to change
+any project data, instead refer to `setup.cfg`.
+"""
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup()
